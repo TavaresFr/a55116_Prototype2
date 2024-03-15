7fa975c06e777fb9d317cf4aa7007dd5c71b816a
TavaresFr <152510118+TavaresFr@users.noreply.github.com>
2024-03-15 02:11:39 +0000

    Get the user’s horizontal input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed, maxX;
+    private float horiz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horiz = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
